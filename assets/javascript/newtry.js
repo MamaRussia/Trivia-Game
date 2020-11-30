6d6292f20f1a316d78f9c92c4d745c30e0957354
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2020-11-30 09:01:41 -0700

    refactoring jquery because jquery sucks. added showQ function that iterates though array of questions

---
+++ assets/javascript/newtry.js
@@ -0,0 +1,140 @@
+const startBtn = document.getElementById('start');
+
+function playAudio() {
+  const x = document.getElementById('myAudio');
+  x.play();
+}
+
+const winnerPhoto = {
+  photo: 'assets/images/winner.gif',
+};
+const loserPhoto = {
+  photo: 'assets/images/losegame.gif',
+};
+const noAnswerPhoto = {
+  photo: 'assets/images/noanswer.gif',
+};
+const incorrectPhotos = [
+  {
+    photo: 'assets/images/incorrect1.gif',
+  },
+  {
+    photo: 'assets/images/incorrect2.gif',
+  },
+  {
+    photo: 'assets/images/incorrect3.gif',
+  },
+  {
+    photo: 'assets/images/incorrect4.gif',
+  },
+  {
+    photo: 'assets/images/incorrect5.gif',
+  },
+  {
+    photo: 'assets/images/incorrect6.gif',
+  },
+  {
+    photo: 'assets/images/incorrect7.gif',
+  },
+  {
+    photo: 'assets/images/incorrect8.gif',
+  },
+  {
+    photo: 'assets/images/incorrect9.gif',
+  },
+  {
+    photo: 'assets/images/incorrect10.gif',
+  },
+];
+const questions = [
+  {
+    question: 'By law, what is banned in Japanese restaurants?',
+    choice: ['Talking', 'Smiling', 'Tipping', 'Farting'],
+    answer: 2,
+    photo: 'assets/images/tipping.gif',
+  },
+  {
+    question: '43% of women want to try S&M after smelling what?',
+    choice: [
+      'Rose Petals',
+      'Vanilla Extract',
+      'Fresh Cut Grass',
+      'Brut Cologne',
+    ],
+    answer: 1,
+    photo: 'assets/images/vanilla.gif',
+  },
+  {
+    question: 'When people are frightened, their ears produce more what?',
+    choice: ['Hair', 'Mites', 'Sweat', 'Earwax'],
+    answer: 3,
+    photo: 'assets/images/earwax.gif',
+  },
+  {
+    question:
+      'What do Krusty the Clown and Chandler in Friends have an extra one of?',
+    choice: ['Nipple', 'Shoe', 'Scooter', 'Sidekick'],
+    answer: 0,
+    photo: 'assets/images/nipple.gif',
+  },
+  {
+    question: 'Men without what are more likely to get Cirrhosis of the liver?',
+    choice: ['Wives', 'Cars', 'Animals', 'Chest Hair'],
+    answer: 3,
+    photo: 'assets/images/chesthair.gif',
+  },
+  {
+    question:
+      '98% of Americans feel better about themselves after they do what?',
+    choice: [
+      'Take a Shower',
+      'Flush the Toilet',
+      'Clean their Home',
+      'Help the Poor',
+    ],
+    answer: 1,
+    photo: 'assets/images/toilet.gif',
+  },
+  {
+    question: 'What does M&M’s stand for?',
+    choice: ['Mmm & Mmmm', 'Mark & Maura', 'Melt & Mash', "Mars & Murrie's"],
+    answer: 3,
+    photo: 'assets/images/MM.gif',
+  },
+  {
+    question: 'What city is further west?',
+    choice: ['Long Beach, CA', 'Reno, NV', 'Los Angeles, CA', 'Riverside, CA'],
+    answer: 1,
+    photo: 'assets/images/reno.gif',
+  },
+  {
+    question:
+      'Who was selected as Glamour magazines “10 Best Dressed College Girls?”',
+    choice: [
+      'Serena Williams',
+      'Ann Coulter',
+      'Martha Stewart',
+      'Kathy Gifford',
+    ],
+    answer: 2,
+    photo: 'assets/images/marthastewart.gif',
+  },
+  {
+    question: 'What Dr. Seuss book uses no more than 50 words?',
+    choice: [
+      'Green Eggs & Ham',
+      'The Cat in the Hat',
+      'If I Ran the Zoo',
+      'Fox in Sox',
+    ],
+    answer: 0,
+    photo: 'assets/images/greeneggs.gif',
+  },
+];
+
+function showQs() {
+    const index = Math.floor(questions.length - 1);
+    const questionSelected = questions[index];
+    questions.splice(index, 1)
+    console.log(questionSelected);
+}
