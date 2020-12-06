ff948fe083ff2b7c8fb33820030f6d03ca78059e
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2020-12-05 22:34:38 -0700

    added time to answer onpage text

--- assets/javascript/newtry.js
+++ assets/javascript/newtry.js
@@ -222,13 +222,13 @@ function decrement() {
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
+        // minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
+        // minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+        display.textContent = `Time to answer:  ${seconds}`;
 
         if (--timer < 0) {
             timer = duration;
